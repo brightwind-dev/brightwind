4390066637f1aa304df9273285d154c0b0eb6f33
stephen <stephen@brightwindanalysis.com>
2018-08-27 13:29:49 +0100

    Add _predict(0 to BulkSpeedRatio

--- analyse/correlation.py
+++ analyse/correlation.py
@@ -223,19 +223,11 @@ class OrdinaryLeastSquares(CorrelBase):
 
 
 class BulkSpeedRatio(CorrelBase):
-    def __init__(self, ref, target, averaging_prd, coverage_threshold, sectors=12,
-                                        cutoff=None, direction_bin_array=None, lt_ref_speed=None, preprocess=True):
-        CorrelBase.__init__(self, ref, target, averaging_prd, coverage_threshold, ref_dir=ref_dir, target_dir=target_dir,
-                            preprocess=preprocess)
+    def __init__(self, ref, target, averaging_prd, coverage_threshold, cutoff=None, preprocess=True):
+        CorrelBase.__init__(self, ref, target, averaging_prd, coverage_threshold, preprocess=preprocess)
         self.params = 'not run yet'
         self.cutoff = cutoff
-        self.direction_bin_array = direction_bin_array
         self._filter()      #Filter low wind speeds
-        self.ref_dir_bins = get_binned_direction_series(self.data['ref_dir'], sectors, direction_bin_array=
-                                                                        self.direction_bin_array).rename('ref_dir_bin')
-        self.data = pd.concat([self.data, self.ref_dir_bins], axis=1, join='inner')
-        self.data = self.data.dropna()
-        self.run()
 
     def _filter(self):
         if self.cutoff is not None:
@@ -243,9 +235,12 @@ class BulkSpeedRatio(CorrelBase):
 
     def run(self):
         self.params = dict()
-        for sector, group in self.data.groupby(['ref_dir_bin']):
-            self.params['Sector '+str(sector)+" slope"] = group['target_spd'].mean()/group['ref_spd'].mean()
+        self.params[" slope"] = self.data['target_spd'].mean()/self.data['ref_spd'].mean()
 
+    def _predict(self, x):
+        def linear_function(x, slope, offset):
+            return (x*slope) + offset
+        return x.transform(linear_function, slope=self.params['slope'], offset=self.params['offset'])
 
 
 class SpeedSort(CorrelBase):
@@ -404,40 +399,3 @@ class SpeedSort(CorrelBase):
                                                         drop_count=True)),self.data['target_spd']], axis=0)
         else:
             return self._predict(input_spd, input_dir)
-
-
-def linear_regression(ref: pd.Series, target: pd.Series, averaging_prd: str, coverage_threshold: float, plot:bool=False):
-    """Accepts two dataframes with timestamps as indexes and averaging period.
-    :param: ref_speed : Dataframe containing reference speed as a column, timestamp as the index.
-    :param: target_speed: Dataframe containing target speed as a column, timestamp as the index.
-    :averaging_prd: Groups data by the period specified by period.
-        2T, 2 min for minutely average
-        Set period to 1D for a daily average, 3D for three hourly average, similarly 5D, 7D, 15D etc.
-        Set period to 1H for hourly average, 3H for three hourly average and so on for 5H, 6H etc.
-        week
-        Set period to 1MS for monthly average
-        Set period to 1AS fo annual average
-    :return :A dictionary containing the following keys, r2, slope, offset and num_data_points
-    """
-
-    ref_processed, target_processed = _preprocess_data_for_correlations(ref, target, averaging_prd, coverage_threshold)
-    common_idxs, data_pts = _common_idxs(ref_processed, target_processed)
-    if plot:
-        _scatter_plot(ref_processed.loc[common_idxs].values,target_processed.loc[common_idxs].values, predicted_y=0,
-                      x_label="Reference Data", y_label="Target Data  ")
-
-
-    # Linear Regression
-    model = LinearRegression()
-    model.fit(ref_processed.loc[common_idxs].values,
-              target_processed.loc[common_idxs].values)
-    r2 = model.score(ref_processed.loc[common_idxs].values,
-                     target_processed.loc[common_idxs].values)
-    prediction = model.predict(ref_processed.loc[common_idxs].values)
-    slope = model.coef_
-    offset = model.intercept_
-    rmse = mean_squared_error(prediction,target_processed.loc[common_idxs].values) **0.5
-    mae = mean_absolute_error(prediction,target_processed.loc[common_idxs].values)
-    # lt_ref_speed = mean_of_monthly_means(ref_speed).mean()
-    # predicted_lt_speed  = calc_target_value_by_linear_model(lt_ref_speed, slope[0], offset[0])
-    return {'num_data_points': data_pts, 'slope': slope, 'offset': offset, 'r2': r2, 'RMSE':rmse, 'MAE':mae}
--- inheritance_class_method_test.py
+++ inheritance_class_method_test.py
@@ -1,7 +1,8 @@
 class Base:
-    def __init__(self, x):
+    def __init__(cls, x):
         print('Initializing base class')
-        self.x = x
+        cls.x = x
+        cls.__init__Directional(x)
     def plot(self):
         print('Plotting from base:', self.x)
     def hell_yeah(self):
@@ -10,27 +11,30 @@ class Base:
     # def directional(cls, x):
     #     obj1 = cls(x)
     #     sectors = [obj1]
-    class Directional(Base):
+    class Directional():
         def __init__(self, sectors):
             print('Initializing direction class')
             self.sectors = sectors
+
         def plot(self):
             for sector_model in self.sectors:
                 print('plotting from directional base')
                 sector_model.plot()
+
         def synthesize(self):
                 return 0
         # return Directional(sectors)
-class Derived(Base):
-    def __init__(self, x, check=True):
+class Derived(Base if directional==False else Base.Directions):
+    def __init__(self, x, directional=True):
         print('Initialising Derived class')
         self.x = x
-        if not check:
-            print("entering if")
-            Base.__init__(self, x)
-        else:
-            print("entering else")
-            Base.Directional.__init__(self, x)
+
+        # if not check:
+        #     print("entering if")
+        #     Base.__init__(self, x)
+        # else:
+        #     print("entering else")
+        #     Base.Directional.__init__(self, x)
         print(dir(self))
 
     def predict(self):
