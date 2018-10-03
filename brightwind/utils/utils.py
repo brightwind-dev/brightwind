7e98cae4df75df9602db487763d9b773f795c07b
Inder95 <singhinderpreet1995@gmail.com>
2018-10-03 15:01:10 +0100

    Add get_direction_bin_array() to utils

--- brightwind/utils/utils.py
+++ brightwind/utils/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def _range_0_to_360(dir):
     if dir < 0:
         return dir+360
@@ -5,3 +8,11 @@ def _range_0_to_360(dir):
         return dir % 360
     else:
         return dir
+
+
+def get_direction_bin_array(sectors):
+    bin_start = 180.0/sectors
+    direction_bins = np.arange(bin_start, 360, 360.0/sectors)
+    direction_bins = np.insert(direction_bins, 0, 0)
+    direction_bins = np.append(direction_bins, 360)
+    return direction_bins
