648100bbe5dc9f43ee3fdbc499e833bdffd1ef03
stephenholleran <stephen.holleran@gmail.com>
2018-11-23 20:50:58 +0000

    change merra2 to merra in datasets

--- brightwind/datasets/__init__.py
+++ brightwind/datasets/__init__.py
@@ -6,7 +6,7 @@ import brightwind as bw
 
 creyap_80m = bw.load_timeseries(bw.datasets.creyap_80m_csv)
 creyap_50m = bw.load_timeseries(bw.datasets.creyap_50m_csv)
-merra2_west = bw.load_timeseries(bw.datasets.merra2_west)
+merra_west = bw.load_timeseries(bw.datasets.merra_west)
 
 """
 __all__ = ['creyap_80m_csv', 'creyap_50m_csv', 'merra2_west']
@@ -15,4 +15,4 @@ import os
 
 creyap_80m_csv = os.path.join(os.path.dirname(__file__), 'Shell_Flats_1_80mHAT.csv')
 creyap_50m_csv = os.path.join(os.path.dirname(__file__), 'Shell_Flats_2_50mHAT.csv')
-merra2_west = os.path.join(os.path.dirname(__file__), 'MERRA_W03.332_N54.000.csv')
+merra_west = os.path.join(os.path.dirname(__file__), 'MERRA_W03.332_N54.000.csv')
