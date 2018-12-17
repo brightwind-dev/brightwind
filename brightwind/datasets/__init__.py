b12ee6c14b2ae67a40b37b5e397a19b97cc66b2b
stephenholleran <stephen.holleran@gmail.com>
2018-12-17 12:26:29 +0000

    update test_datasets with re-organised datasets

--- brightwind/datasets/__init__.py
+++ brightwind/datasets/__init__.py
@@ -4,15 +4,15 @@ Example datasets that can be used with the library.
 Usage:
 import brightwind as bw
 
-creyap_80m = bw.load_timeseries(bw.datasets.creyap_80m_csv)
-creyap_50m = bw.load_timeseries(bw.datasets.creyap_50m_csv)
-merra_west = bw.load_timeseries(bw.datasets.merra_west)
+creyap_80m = bw.load_csv(bw.datasets.creyap_80m_csv)
+creyap_50m = bw.load_csv(bw.datasets.creyap_50m_csv)
+merra_west = bw.load_csv(bw.datasets.merra_west)
 
 """
-__all__ = ['creyap_80m_csv', 'creyap_50m_csv', 'merra2_west']
-
 import os
 
-creyap_80m_csv = os.path.join(os.path.dirname(__file__), 'Shell_Flats_1_80mHAT.csv')
-creyap_50m_csv = os.path.join(os.path.dirname(__file__), 'Shell_Flats_2_50mHAT.csv')
-merra_west = os.path.join(os.path.dirname(__file__), 'MERRA_W03.332_N54.000.csv')
+__all__ = ['shell_flats_80m_csv', 'shell_flats_50m_csv', 'shell_flats_merra']
+
+shell_flats_80m_csv = os.path.join(os.path.dirname(__file__), 'offshore-CREYAP-2-data-pack', 'Shell_Flats_1_80mHAT.csv')
+shell_flats_50m_csv = os.path.join(os.path.dirname(__file__), 'offshore-CREYAP-2-data-pack', 'Shell_Flats_2_50mHAT.csv')
+shell_flats_merra = os.path.join(os.path.dirname(__file__), 'offshore-CREYAP-2-data-pack', 'MERRA_W03.332_N54.000.csv')
--- brightwind/tests/test_datasets.py
+++ brightwind/tests/test_datasets.py
@@ -4,7 +4,7 @@ import brightwind.datasets
 
 
 def test_creyap():
-    load_csv(brightwind.datasets.creyap_80m_csv)
-    load_csv(brightwind.datasets.creyap_50m_csv)
-    load_csv(brightwind.datasets.merra2_west)
+    load_csv(brightwind.datasets.shell_flats_80m_csv)
+    load_csv(brightwind.datasets.shell_flats_50m_csv)
+    load_csv(brightwind.datasets.shell_flats_merra)
 
