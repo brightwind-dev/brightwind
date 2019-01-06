22c45a16a4faba7c84862e17ed677b2ecb435932
Inder <singhinderpreet1995@gmail.com>
2019-01-06 22:35:53 +0000

    Add tests for Sector Ration by sector.

--- brightwind/tests/test_analyse.py
+++ brightwind/tests/test_analyse.py
@@ -1,5 +1,6 @@
 import pytest
-from ..analyse.analyse import monthly_means
+from ..analyse.analyse import monthly_means, SectorRatio
+
 from ..load.load import load_csv
 import brightwind.datasets
 
@@ -12,4 +13,10 @@ def test_monthly_means():
                         return_data=True)
     monthly_means(load_csv(brightwind.datasets.shell_flats_80m_csv).WS80mWS425NW_Avg)
     monthly_means(load_csv(brightwind.datasets.shell_flats_80m_csv).WS80mWS425NW_Avg, return_data=True)
-    assert True == True
+    assert True
+
+def test_sector_ratio_by_sector():
+    data = load_csv(brightwind.datasets.shell_flats_80m_csv)
+    SectorRatio.by_sector(data['WS70mA100NW_Avg'], data['WS70mA100SE_Avg'], data['WD50mW200PNW_VAvg'],
+                          sectors = 72, boom_dir_1 = 315, boom_dir_2 = 135,return_data=True)[1]
+    assert True
--- brightwind/tests/test_transform.py
+++ brightwind/tests/test_transform.py
@@ -5,6 +5,6 @@ def test_adjust_slope_offset():
 
     #Write the tests here
     #The function has been imported and use it as you would normally use
-
+#
 
     assert True
