2114a25478a92cc12d09363835dd22c49cc6c765
Inder <singhinderpreet1995@gmail.com>
2019-01-06 21:52:58 +0000

    Add tests for transform.

---
+++ brightwind/tests/test_transform.py
@@ -0,0 +1,10 @@
+import pytest
+from ..transform.transform import adjust_slope_offset
+
+def test_adjust_slope_offset():
+
+    #Write the tests here
+    #The function has been imported and use it as you would normally use
+
+
+    assert True
