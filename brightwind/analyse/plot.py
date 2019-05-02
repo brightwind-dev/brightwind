e886aa61a72d69d9d547f4e355d97a2e97a77d2d
Inder <singhinderpreet1995@gmail.com>
2019-05-02 17:35:44 +0100

    Center text below plot in plot_sector_ratio. Iss #44

--- brightwind/analyse/plot.py
+++ brightwind/analyse/plot.py
@@ -343,7 +343,7 @@ def plot_sector_ratio(sec_ratio, wdir, sec_ratio_dist, col_names, boom_dir_1=-1,
 
     if ctr == 2:
         ax.annotate('*Plot generated using ' + col_names[1] + ' (yellow green boom) divided by' + col_names[0] +
-                    ' (blue boom)', xy=(20, 10), xycoords='figure pixels')
+                    ' (blue boom)', xy=(0.17, 0.01), xycoords='figure fraction')
     ax.scatter(np.radians(wdir), sec_ratio, color=bw_colors('asphault'), alpha=0.3, s=1)
     return ax.get_figure()
 
