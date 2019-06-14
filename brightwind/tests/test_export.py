e3ef85720cdc142996a7ebdab27c4a0c01692aa1
Luke Cunningham <lucunnin@tcd.ie>
2019-06-14 10:49:56 +0100

    update input parameters in test_export_tab_file

--- brightwind/tests/test_export.py
+++ brightwind/tests/test_export.py
@@ -1,7 +1,8 @@
 import pytest
 import brightwind as bw
 
+
 def test_export_tab_file():
     df = bw.load_campbell_scientific(bw.datasets.demo_campbell_scientific_site_data)
     graph, tab = bw.freq_table(df.Spd40mN, df.Dir38mS, return_data=True)
-    bw.export_tab_file(tab, name='campbell_tab_file', lat=10, long=10)
+    bw.export_tab_file(tab, 80, 10, 10, file_name='campbell_tab_file')
