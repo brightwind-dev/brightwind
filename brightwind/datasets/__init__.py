36a254b9f7570ce229465ca59af98c849147fcd5
BiancaMorandi <biamorandi@gmail.com>
2020-06-03 12:44:04 +0100

    iss #228 add test of fixed function

--- brightwind/datasets/__init__.py
+++ brightwind/datasets/__init__.py
@@ -2,7 +2,8 @@
 import os
 
 __all__ = ['demo_data', 'demo_campbell_scientific_data', 'demo_merra2_NW',
-           'demo_merra2_NE', 'demo_merra2_SE', 'demo_merra2_SW', 'demo_windographer_data']
+           'demo_merra2_NE', 'demo_merra2_SE', 'demo_merra2_SW', 'demo_windographer_data',
+           'demo_windographer_flagging_log', 'demo_windographer_flagging_log2', 'demo_cleaning_file']
 
 demo_campbell_scientific_data = os.path.join(os.path.dirname(__file__), 'demo', 'campbell_scientific_demo_data.csv')
 demo_data = os.path.join(os.path.dirname(__file__), 'demo', 'demo_data.csv')
@@ -12,6 +13,10 @@ demo_merra2_NW = os.path.join(os.path.dirname(__file__), 'demo', 'MERRA-2_NW_200
 demo_merra2_NE = os.path.join(os.path.dirname(__file__), 'demo', 'MERRA-2_NE_2000-01-01_2017-06-30.csv')
 demo_merra2_SE = os.path.join(os.path.dirname(__file__), 'demo', 'MERRA-2_SE_2000-01-01_2017-06-30.csv')
 demo_merra2_SW = os.path.join(os.path.dirname(__file__), 'demo', 'MERRA-2_SW_2000-01-01_2017-06-30.csv')
+
+demo_windographer_flagging_log = os.path.join(os.path.dirname(__file__), 'demo', 'windographer_flagging_log.txt')
+demo_windographer_flagging_log2 = os.path.join(os.path.dirname(__file__), 'demo', 'windographer_flagging_log2.txt')
+demo_cleaning_file = os.path.join(os.path.dirname(__file__), 'demo', 'demo_cleaning_file.csv')
 
 
 def datasets_available():
