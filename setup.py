9a16dd09d93808ac15b50717bb1f197874ce27ee
stephenholleran <stephen@brightwindanalysis.com>
2020-06-19 12:33:31 +0100

    iss #200 pull version from __init__.py

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 from setuptools import setup, find_packages
+import codecs
+import os.path
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
 setup(
     name='brightwind',
     # Update version number here, below and in _init_.py:
-    version='1.0.0',
+    version=get_version("brightwind/__init__.py"),
     packages=['brightwind', 'brightwind.load', 'brightwind.utils', 'brightwind.export', 'brightwind.analyse',
               'brightwind.transform'],  # , 'brightwind.datasets'],
     package_data={
