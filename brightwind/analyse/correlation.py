5f91c264c4a6cf43e4439ccb8205a3b411f04668
BiancaMorandi <biamorandi@gmail.com>
2020-09-28 17:52:23 +0100

    iss #246 undo update SpeedSort.synthetize function

--- brightwind/analyse/correlation.py
+++ brightwind/analyse/correlation.py
@@ -508,16 +508,9 @@ class SpeedSort(CorrelBase):
         # This will give erroneous result when the averaging period is not a whole number such that ref and target does
         # bot get aligned -Inder
         if input_spd is None and input_dir is None:
-            if 'D' in self.averaging_prd:
-                avg_prd_sec = int(''.join(c for c in self.averaging_prd if (c.isdigit()))) * 24 * 60 * 60
-                step_backward = int(
-                    (self.target_spd.index[0] - self.ref_spd.index[0]).total_seconds() / avg_prd_sec)
-                ref_start_date = self.target_spd.index[0] - datetime.timedelta(seconds=step_backward * avg_prd_sec)
-            else:
-                ref_start_date = self.ref_spd.index[0]
-            output = self._predict(tf.average_data_by_period(self.ref_spd[ref_start_date:], self.averaging_prd,
+            output = self._predict(tf.average_data_by_period(self.ref_spd, self.averaging_prd,
                                                              return_coverage=False),
-                                   tf.average_data_by_period(self.ref_dir[ref_start_date:], self.averaging_prd,
+                                   tf.average_data_by_period(self.ref_dir, self.averaging_prd,
                                                              return_coverage=False))
             output = tf.average_data_by_period(self.target_spd, self.averaging_prd,
                                                return_coverage=False).combine_first(output)
