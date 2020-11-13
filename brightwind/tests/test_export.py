613bf477e0a3883ba830d4a61661deafaf5a8695
stephenholleran <stephen@brightwindanalysis.com>
2020-11-13 19:00:36 +0000

    iss #163 clean test_export

--- brightwind/tests/test_export.py
+++ brightwind/tests/test_export.py
@@ -1,20 +1,20 @@
 import pytest
 import brightwind as bw
 
+DATA = bw.load_csv(bw.datasets.demo_data)
+DATA = bw.apply_cleaning(DATA, bw.datasets.demo_cleaning_file)
+
 
 def test_export_tab_file():
-    df = bw.load_campbell_scientific(bw.datasets.demo_campbell_scientific_site_data)
-    graph, tab = bw.freq_table(df.Spd40mN, df.Dir38mS, return_data=True)
+    graph, tab = bw.freq_table(DATA.Spd40mN, DATA.Dir38mS, return_data=True)
     bw.export_tab_file(tab, 80, 10, 10, file_name='export_tab_file.tab')
-    graph, tab = bw.freq_table(df[['Spd40mN']], df[['Dir38mS']], return_data=True)
+    graph, tab = bw.freq_table(DATA[['Spd40mN']], DATA[['Dir38mS']], return_data=True)
     bw.export_tab_file(tab, 80, 10, 10, file_name='export_tab_file_1.tab')
+    assert True
 
 
 def test_export_to_csv():
+    bw.export_csv(DATA, file_name='export_to_csv')
 
-    df = bw.load_campbell_scientific(bw.datasets.demo_campbell_scientific_site_data)
-
-    bw.export_csv(df, file_name='export_to_csv')
-
-    bw.export_csv(df, file_name='export_to_csv_tab.tab', sep='\t')
-
+    bw.export_csv(DATA, file_name='export_to_csv_tab.tab', sep='\t')
+    assert True
