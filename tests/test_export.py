200c328a7c749b57c1e7560b1db13052e6c129ce
stephenholleran <stephen@brightwindanalysis.com>
2020-11-13 20:35:53 +0000

    iss #163 use temp folder for exporting files

--- tests/test_export.py
+++ tests/test_export.py
@@ -6,15 +6,17 @@ DATA = bw.apply_cleaning(DATA, bw.datasets.demo_cleaning_file)
 
 
 def test_export_tab_file():
+    folder = r'temp'
     graph, tab = bw.freq_table(DATA.Spd40mN, DATA.Dir38mS, return_data=True)
-    bw.export_tab_file(tab, 80, 10, 10, file_name='export_tab_file.tab')
+    bw.export_tab_file(tab, 80, 10, 10, file_name='export_tab_file.tab', folder_path=folder)
     graph, tab = bw.freq_table(DATA[['Spd40mN']], DATA[['Dir38mS']], return_data=True)
-    bw.export_tab_file(tab, 80, 10, 10, file_name='export_tab_file_1.tab')
+    bw.export_tab_file(tab, 80, 10, 10, file_name='export_tab_file_1.tab', folder_path=folder)
     assert True
 
 
 def test_export_to_csv():
-    bw.export_csv(DATA, file_name='export_to_csv')
+    folder = r'temp'
+    bw.export_csv(DATA, file_name='export_to_csv', folder_path=folder)
 
-    bw.export_csv(DATA, file_name='export_to_csv_tab.tab', sep='\t')
+    bw.export_csv(DATA, file_name='export_to_csv_tab.tab', folder_path=folder, sep='\t')
     assert True
