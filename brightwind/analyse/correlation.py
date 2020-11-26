93b9776d0a0a7e0132b7b1f39ee05c83e0faee73
stephenholleran <stephen@brightwindanalysis.com>
2020-11-26 18:09:30 +0000

    iss #261 keep ref and tar datasets names in correlations

--- brightwind/analyse/correlation.py
+++ brightwind/analyse/correlation.py
@@ -33,7 +33,7 @@ __all__ = ['']
 
 
 class CorrelBase:
-    def __init__(self, ref_spd, target_spd, averaging_prd, coverage_threshold, ref_dir=None,
+    def __init__(self, ref_spd, target_spd, averaging_prd, coverage_threshold=None, ref_dir=None,
                  target_dir=None, preprocess=True):
         self.ref_spd = ref_spd
         self.ref_dir = ref_dir
@@ -42,15 +42,16 @@ class CorrelBase:
         self.averaging_prd = averaging_prd
         self.coverage_threshold = coverage_threshold
         self.preprocess = preprocess
+        # Get the name of the columns so they can be passed around
+        self._ref_spd_col_name = ref_spd.name if ref_spd is not None else None  # MIGHT BREAK FOR MultipleLinearRegression WHEN A LIST OF SERIES IS SENT.
+        self._ref_dir_col_name = ref_dir.name if ref_dir is not None else None
+        self._tar_spd_col_name = target_spd.name if target_spd is not None else None
+        self._tar_dir_col_name = target_dir.name if target_dir is not None else None
         if preprocess:
             self.data = CorrelBase._averager(ref_spd, target_spd, averaging_prd, coverage_threshold,
                                              ref_dir, target_dir)
         else:
             self.data = pd.concat([ref_spd, target_spd, ref_dir, target_dir], axis=1, join='inner')
-        if ref_dir is None and target_dir is None:
-            self.data.columns = ['ref_spd', 'target_spd']
-        else:
-            self.data.columns = ['ref_spd', 'target_spd', 'ref_dir', 'target_dir']
         self.num_data_pts = len(self.data)
 
     @staticmethod
@@ -70,8 +71,10 @@ class CorrelBase:
 
     def plot(self, title=""):
         """For plotting"""
-        return _scatter_plot(self.data['ref_spd'].values.flatten(), self.data['target_spd'].values.flatten(),
-                             self._predict(self.data['ref_spd']).values.flatten())
+        return _scatter_plot(self.data[self._ref_spd_col_name].values.flatten(),
+                             self.data[self._tar_spd_col_name].values.flatten(),
+                             self._predict(self.data[self._ref_spd_col_name]).values.flatten(),
+                             x_label=self._ref_spd_col_name, y_label=self._tar_spd_col_name)
 
     def synthesize(self, ext_input=None):
         # This will give erroneous result when the averaging period is not a whole number such that ref and target does
@@ -92,8 +95,8 @@ class CorrelBase:
 
     def get_r2(self):
         """Returns the r2 score of the model"""
-        return 1.0 - (sum((self.data['target_spd'] - self._predict(self.data['ref_spd'])) ** 2) / (
-            sum((self.data['target_spd'] - self.data['target_spd'].mean()) ** 2)))
+        return 1.0 - (sum((self.data[self._tar_spd_col_name] - self._predict(self.data[self._ref_spd_col_name])) ** 2) /
+                      (sum((self.data[self._tar_spd_col_name] - self.data[self._tar_spd_col_name].mean()) ** 2)))
 
     def get_error_metrics(self):
         return 0
@@ -136,8 +139,8 @@ class OrdinaryLeastSquares(CorrelBase):
         return 'Ordinary Least Squares Model' + str(self.params)
 
     def run(self, show_params=True):
-        p, res = lstsq(np.nan_to_num(self.data['ref_spd'].values.flatten()[:, np.newaxis] ** [1, 0]),
-                       np.nan_to_num(self.data['target_spd'].values.flatten()))[0:2]
+        p, res = lstsq(np.nan_to_num(self.data[self._ref_spd_col_name].values.flatten()[:, np.newaxis] ** [1, 0]),
+                       np.nan_to_num(self.data[self._tar_spd_col_name].values.flatten()))[0:2]
 
         self.params = {'slope': p[0], 'offset': p[1]}
         self.params['r2'] = self.get_r2()
@@ -183,9 +186,10 @@ class OrthogonalLeastSquares(CorrelBase):
         return 'Orthogonal Least Squares Model ' + str(self.params)
 
     def run(self, show_params=True):
-        fit_data = RealData(self.data['ref_spd'].values.flatten(), self.data['target_spd'].values.flatten())
-        p, res = lstsq(np.nan_to_num(fit_data.x[:, np.newaxis] ** [1, 0]), np.nan_to_num(np.asarray(fit_data.y)
-                                                                                         [:, np.newaxis]))[0:2]
+        fit_data = RealData(self.data[self._ref_spd_col_name].values.flatten(), 
+                            self.data[self._tar_spd_col_name].values.flatten())
+        p, res = lstsq(np.nan_to_num(fit_data.x[:, np.newaxis] ** [1, 0]), 
+                       np.nan_to_num(np.asarray(fit_data.y)[:, np.newaxis]))[0:2]
         self._model = ODR(fit_data, Model(OrthogonalLeastSquares.linear_func), beta0=[p[0][0], p[1][0]])
         self.out = self._model.run()
         self.params = {'slope': self.out.beta[0], 'offset': self.out.beta[1]}
@@ -215,7 +219,8 @@ class MultipleLinearRegression(CorrelBase):
                 axis=1, join='inner')
         else:
             self.data = pd.concat(list(self.ref_spd, self.target_spd), axis=1, join='inner')
-        self.data.columns = ['ref_spd_' + str(i + 1) for i in range(0, len(self.ref_spd.columns))] + ['target_spd']
+        self.data.columns = ['ref_spd_' + str(i + 1) for i in range(0, len(self.ref_spd.columns))] + \
+                            [self._tar_spd_col_name]
         self.data = self.data.dropna()
         self.params = 'not run yet'
 
@@ -223,8 +228,9 @@ class MultipleLinearRegression(CorrelBase):
         return 'Multiple Linear Regression Model ' + str(self.params)
 
     def run(self, show_params=True):
-        p, res = lstsq(np.column_stack((self.data.iloc[:, :len(self.data.columns) - 1].values,
-                                        np.ones(len(self.data)))), self.data['target_spd'].values.flatten())[0:2]
+        p, res = lstsq(np.column_stack((self.data.iloc[:, :len(self.data.columns) - 1].values, 
+                                        np.ones(len(self.data)))), 
+                       self.data[self._tar_spd_col_name].values.flatten())[0:2]
         self.params = {'slope': p[:-1], 'offset': p[-1]}
         if show_params:
             self.show_params()
@@ -243,13 +249,14 @@ class MultipleLinearRegression(CorrelBase):
             return pd.concat([self._predict(tf.average_data_by_period(self.ref_spd.loc[:min(self.data.index)],
                                                                       self.averaging_prd,
                                                                       return_coverage=False)),
-                              self.data['target_spd']], axis=0)
+                              self.data[self._tar_spd_col_name]], axis=0)
         else:
             return self._predict(ext_input)
 
     def get_r2(self):
-        return 1.0 - (sum((self.data['target_spd'] - self._predict(self.data.drop(['target_spd'], axis=1))) ** 2) / (
-            sum((self.data['target_spd'] - self.data['target_spd'].mean()) ** 2)))
+        return 1.0 - (sum((self.data[self._tar_spd_col_name] - 
+                           self._predict(self.data.drop([self._tar_spd_col_name], axis=1))) ** 2) / 
+                      (sum((self.data[self._tar_spd_col_name] - self.data[self._tar_spd_col_name].mean()) ** 2)))
 
     def plot(self):
         return "Cannot plot Multiple Linear Regression"
@@ -277,7 +284,7 @@ class SimpleSpeedRatio(CorrelBase):
 
     def run(self, show_params=True):
         self.params = dict()
-        self.params["ratio"] = self.data['target_spd'].mean() / self.data['ref_spd'].mean()
+        self.params["ratio"] = self.data[self._tar_spd_col_name].mean() / self.data[self._ref_spd_col_name].mean()
         if show_params:
             self.show_params()
 
@@ -323,7 +330,8 @@ class SpeedSort(CorrelBase):
             return x.transform(linear_function, slope=self.params['slope'], offset=self.params['offset'])
 
         def plot_model(self, title):
-            _scatter_plot(sorted(self.sector_ref.values.flatten()), sorted(self.sector_target.values.flatten()),
+            _scatter_plot(sorted(self.sector_ref.values.flatten()),
+                          sorted(self.sector_target.values.flatten()),
                           sorted(self.sector_predict(self.sector_ref).values.flatten()))
 
     def __init__(self, ref_spd, ref_dir, target_spd, target_dir, averaging_prd, coverage_threshold=0.9, sectors=12,
@@ -335,18 +343,18 @@ class SpeedSort(CorrelBase):
         if direction_bin_array is not None:
             self.sectors = len(direction_bin_array)-1
         if lt_ref_speed is None:
-            self.lt_ref_speed = momm(self.data['ref_spd'])
+            self.lt_ref_speed = momm(self.data[self._ref_spd_col_name])
         else:
             self.lt_ref_speed = lt_ref_speed
         self.cutoff = min(0.5 * self.lt_ref_speed, 4.0)
-        self.ref_veer_cutoff = self._get_veer_cutoff(self.data['ref_spd'])
-        self.target_veer_cutoff = self._get_veer_cutoff((self.data['target_spd']))
+        self.ref_veer_cutoff = self._get_veer_cutoff(self.data[self._ref_spd_col_name])
+        self.target_veer_cutoff = self._get_veer_cutoff((self.data[self._tar_spd_col_name]))
         self._randomize_calm_periods()
         self._get_overall_veer()
         # for low ref_speed and high target_speed recalculate direction sector
         self._adjust_low_reference_speed_dir()
 
-        self.ref_dir_bins = _binned_direction_series(self.data['ref_dir'], sectors,
+        self.ref_dir_bins = _binned_direction_series(self.data[self._ref_dir_col_name], sectors,
                                                      direction_bin_array=self.direction_bin_array).rename('ref_dir_bin')
         self.data = pd.concat([self.data, self.ref_dir_bins], axis=1, join='inner')
         self.data = self.data.dropna()
@@ -356,21 +364,23 @@ class SpeedSort(CorrelBase):
         return 'Speed Sort Model ' + str(self.params)
 
     def _randomize_calm_periods(self):
-        idxs = self.data[self.data['ref_spd'] < 1].index
-        self.data.loc[idxs, 'ref_dir'] = 360.0 * np.random.random(size=len(idxs))
-        idxs = self.data[self.data['target_spd'] < 1].index
-        self.data.loc[idxs, 'target_dir'] = 360.0 * np.random.random(size=len(idxs))
+        idxs = self.data[self.data[self._ref_spd_col_name] < 1].index
+        self.data.loc[idxs, self._ref_dir_col_name] = 360.0 * np.random.random(size=len(idxs))
+        idxs = self.data[self.data[self._tar_spd_col_name] < 1].index
+        self.data.loc[idxs, self._tar_dir_col_name] = 360.0 * np.random.random(size=len(idxs))
 
     def _get_overall_veer(self):
-        idxs = self.data[(self.data['ref_spd'] >= self.ref_veer_cutoff) & (self.data['target_spd'] >=
-                                                                           self.target_veer_cutoff)].index
-        self.overall_veer = self._get_veer(self.data.loc[idxs, 'ref_dir'], self.data.loc[idxs, 'target_dir']).mean()
+        idxs = self.data[(self.data[self._ref_spd_col_name] >= self.ref_veer_cutoff) &
+                         (self.data[self._tar_spd_col_name] >= self.target_veer_cutoff)].index
+        self.overall_veer = self._get_veer(self.data.loc[idxs, self._ref_dir_col_name],
+                                           self.data.loc[idxs, self._tar_dir_col_name]).mean()
 
     def _adjust_low_reference_speed_dir(self):
-        idxs = self.data[(self.data['ref_spd'] < 2) & (self.data['target_spd'] > (self.data['ref_spd'] + 4))].index
+        idxs = self.data[(self.data[self._ref_spd_col_name] < 2) &
+                         (self.data[self._tar_spd_col_name] > (self.data[self._ref_spd_col_name] + 4))].index
 
-        self.data.loc[idxs, 'ref_dir'] = (self.data.loc[idxs, 'target_dir'] - self.overall_veer).apply(
-            utils._range_0_to_360)
+        self.data.loc[idxs, self._ref_dir_col_name] = (self.data.loc[idxs, self._tar_dir_col_name] -
+                                                       self.overall_veer).apply(utils._range_0_to_360)
 
     @staticmethod
     def _get_veer_cutoff(speed_col):
@@ -390,10 +400,11 @@ class SpeedSort(CorrelBase):
         return v.apply(change_range)
 
     def _avg_veer(self, sector_data):
-        sector_data = sector_data[(sector_data['ref_spd'] >= self.ref_veer_cutoff) & (sector_data['target_spd'] >=
-                                                                                      self.target_veer_cutoff)]
-        return {'average_veer': self._get_veer(sector_data['ref_dir'], sector_data['target_dir']).mean(),
-                'num_pts_for_veer': len(sector_data['ref_dir'])}
+        sector_data = sector_data[(sector_data[self._ref_spd_col_name] >= self.ref_veer_cutoff) &
+                                  (sector_data[self._tar_spd_col_name] >= self.target_veer_cutoff)]
+        return {'average_veer': self._get_veer(sector_data[self._ref_dir_col_name],
+                                               sector_data[self._tar_dir_col_name]).mean(),
+                'num_pts_for_veer': len(sector_data[self._ref_dir_col_name])}
 
     def run(self, show_params=True):
         self.params = dict()
@@ -405,8 +416,8 @@ class SpeedSort(CorrelBase):
         self.speed_model = dict()
         for sector, group in self.data.groupby(['ref_dir_bin']):
             # print('Processing sector:', sector)
-            self.speed_model[sector] = SpeedSort.SectorSpeedModel(ref_spd=group['ref_spd'],
-                                                                  target_spd=group['target_spd'],
+            self.speed_model[sector] = SpeedSort.SectorSpeedModel(ref_spd=group[self._ref_spd_col_name],
+                                                                  target_spd=group[self._tar_spd_col_name],
                                                                   lt_ref_speed=self.lt_ref_speed)
             self.params[sector] = {'slope': self.speed_model[sector].params['slope'],
                                    'offset': self.speed_model[sector].params['offset'],
@@ -493,9 +504,11 @@ class SpeedSort(CorrelBase):
 
         # _scatter_plot(self.ref_dir, self.target_dir,title='original data')
         _scatter_plot(
-            self.data['ref_dir'][(self.data['ref_spd'] > self.cutoff) & (self.data['target_spd'] > self.cutoff)],
-            self.data['target_dir'][(self.data['ref_spd'] > self.cutoff) & (self.data['target_spd'] > self.cutoff)],
-            x_label='Reference direction', y_label="Target direction")
+            self.data[self._ref_dir_col_name][(self.data[self._ref_spd_col_name] > self.cutoff) &
+                                              (self.data[self._tar_spd_col_name] > self.cutoff)],
+            self.data[self._tar_dir_col_name][(self.data[self._ref_spd_col_name] > self.cutoff) &
+                                              (self.data[self._tar_spd_col_name] > self.cutoff)],
+            x_label=self._ref_dir_col_name, y_label=self._tar_dir_col_name)
 
 
 class SVR(CorrelBase):
@@ -510,18 +523,20 @@ class SVR(CorrelBase):
         return 'Support Vector Regression Model ' + str(self.params)
 
     def run(self, show_params=True):
-        if len(self.data['ref_spd'].values.shape) == 1:
-            x = self.data['ref_spd'].values.reshape(-1, 1)
+        if len(self.data[self._ref_spd_col_name].values.shape) == 1:
+            x = self.data[self._ref_spd_col_name].values.reshape(-1, 1)
         else:
-            x = self.data['ref_spd'].values
-        self.model.fit(x, self.data['target_spd'].values.flatten())
+            x = self.data[self._ref_spd_col_name].values
+        self.model.fit(x, self.data[self._tar_spd_col_name].values.flatten())
         self.params = dict()
-        self.params['RMSE'] = -1 * sklearn_cross_val_score(self.model, x, self.data['target_spd'].values.flatten(),
+        self.params['RMSE'] = -1 * sklearn_cross_val_score(self.model, x,
+                                                           self.data[self._tar_spd_col_name].values.flatten(),
                                                            scoring='neg_mean_squared_error', cv=3)
-        self.params['MAE'] = -1 * sklearn_cross_val_score(self.model, x, self.data['target_spd'].values.flatten(),
+        self.params['MAE'] = -1 * sklearn_cross_val_score(self.model, x,
+                                                          self.data[self._tar_spd_col_name].values.flatten(),
                                                           scoring='neg_mean_absolute_error', cv=3)
         self.params['Explained Variance'] = -1 * sklearn_cross_val_score(self.model, x,
-                                                                         self.data['target_spd'].values.flatten(),
+                                                                         self.data[self._tar_spd_col_name].values.flatten(),
                                                                          scoring='explained_variance', cv=3)
         if show_params:
             self.show_params()
@@ -542,5 +557,7 @@ class SVR(CorrelBase):
 
     def plot(self, title=""):
         """For plotting"""
-        _scatter_plot(self.data['ref_spd'].values.flatten(), self.data['target_spd'].values.flatten(),
-                      self._predict(self.data['ref_spd']).values.flatten(), prediction_marker='.')
+        _scatter_plot(self.data[self._ref_spd_col_name].values.flatten(),
+                      self.data[self._tar_spd_col_name].values.flatten(),
+                      self._predict(self.data[self._ref_spd_col_name]).values.flatten(), prediction_marker='.',
+                      x_label=self._ref_spd_col_name, y_label=self._tar_spd_col_name)
