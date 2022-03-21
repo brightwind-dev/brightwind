1351d1b7eeb4b0654515f6a17ceb0d7a9ce3b744
BiancaMorandi <biamorandi@gmail.com>
2022-03-21 14:41:18 +0000

    iss #314 fix reference path

--- tests/test_export.py
+++ tests/test_export.py
@@ -1,11 +1,16 @@
 import pytest
 import brightwind as bw
+import os
 
 DATA = bw.load_csv(bw.demo_datasets.demo_data)
 DATA = bw.apply_cleaning(DATA, bw.demo_datasets.demo_cleaning_file)
 
+cwd = os.getcwd()
+os.makedirs(os.path.join(cwd, 'temp'), exist_ok=True)
+
 
 def test_export_tab_file():
+    # test with file path provided
     folder = r'temp'
     graph, tab = bw.freq_table(DATA.Spd40mN, DATA.Dir38mS, return_data=True)
     bw.export_tab_file(tab, 80, 10, 10, file_name='export_tab_file.tab', folder_path=folder)
