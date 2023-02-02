c7f81ca1e36cd788517daca42e65b35e49588482
BiancaMorandi <biamorandi@gmail.com>
2023-02-02 11:44:28 +0000

    iss #355 added tests

--- tests/test_load.py
+++ tests/test_load.py
@@ -61,3 +61,33 @@ def test_load_campbell_scientific():
 
     assert (data['2016-01-09 15:30:00':'2016-01-10 23:50:00'].fillna(-999) ==
             data1['2016-01-09 15:30:00':'2016-01-10 23:50:00'].fillna(-999)).all().all()
+
+
+def test_load_brighthub():
+
+    plant_uuid = '715e5518-4f3a-4eb2-87cf-69547789232a'
+    measurement_station_uuid = '0cfd9da4-19d3-4cac-b5ab-4b8f35bac19b'
+    test_period_demo_data = {'start_date': '2009-12-01T17:40:00', 'end_date': '2012-03-10T00:00:00'}
+
+    # To get a specific plant
+    assert bw.LoadBrightHub.get_plants(plant_uuid=plant_uuid)[
+               'plant_type_id'].values[0] == 'onshore_wind'
+
+    # To get a specific measurement station
+    assert (bw.LoadBrightHub.get_measurement_stations(plant_uuid=plant_uuid).dropna(
+        axis=1) == bw.LoadBrightHub.get_measurement_stations(measurement_station_uuid=measurement_station_uuid
+                                                             ).dropna(axis=1)).all().all()
+
+    # To get the data model for a specific measurement station
+    assert bw.LoadBrightHub.get_data_model(measurement_station_uuid=measurement_station_uuid
+                                           )['author'] == 'Brighthub'
+
+    # To get start and end date of data for a specific measurement station
+    assert bw.LoadBrightHub.get_start_end_dates(measurement_station_uuid=measurement_station_uuid
+                                                ) == test_period_demo_data
+
+    # To get data for a specific time period for a specific measurement station
+    data_columns = bw.LoadBrightHub.get_data(measurement_station_uuid=measurement_station_uuid, date_from='2009-12-01',
+                                             date_to='2010-01-01').columns
+    for col in ['Spd1_60m270', 'Spd2_60m180', 'Dir1_58m']:
+        assert col in data_columns
