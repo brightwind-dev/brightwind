551cbbad6c9fcc1f3ca458daa671bdb22aa530e6
BiancaMorandi <biamorandi@gmail.com>
2023-05-23 17:13:50 +0100

    iss #385 update slice_data default input

--- brightwind/utils/utils.py
+++ brightwind/utils/utils.py
@@ -56,18 +56,18 @@ def validate_coverage_threshold(coverage_threshold):
     return coverage_threshold
 
 
-def slice_data(data, date_from='', date_to=''):
+def slice_data(data, date_from=None, date_to=None):
     """
     Returns the slice of data between the two date or datetime ranges.
 
     :param data:        Pandas DataFrame or Series with timestamp as index.
     :type data:         pandas.DataFrame or pandas.Series
-    :param date_from:   Start date as string in format YYYY-MM-DD or YYYY-MM-DD HH:DD. If format of input start date is
+    :param date_from:   Start date as string in format YYYY-MM-DD or YYYY-MM-DD HH:DD. If format of date_from is
                         YYYY-MM-DD, then the first timestamp of the date is used (e.g if date_from=2023-01-01 then
                         2023-01-01 00:00 is the first timestamp of the sliced data). If date_from is not given then
                         the sliced data are taken up to the first timestamp of the dataset.
     :type:              str
-    :param date_to:     End date as string in format YYYY-MM-DD or YYYY-MM-DD HH:DD. If fomat of input end date is
+    :param date_to:     End date as string in format YYYY-MM-DD or YYYY-MM-DD HH:DD. If format date_to is
                         YYYY-MM-DD, then the last timestamp of the previous day is used (e.g if date_to=2023-02-01 then
                         2023-01-31 23:50 is the last timestamp of the sliced data). If date_to is not given then
                         the sliced data are taken up to the last timestamp of the dataset.
@@ -90,19 +90,17 @@ def slice_data(data, date_from='', date_to=''):
         data_sliced = bw.utils.utils.slice_data(DATA, date_from='2016-11-23')
 
     """
-    date_from = pd.to_datetime(date_from, format="%Y-%m-%d %H:%M")
-    if date_to is '':
-        date_to = pd.to_datetime(date_to, format="%Y-%m-%d")
+    if pd.isnull(date_from):
+        date_from = data.index[0]
+    else:
+        date_from = pd.to_datetime(date_from, format="%Y-%m-%d %H:%M")
+
+    if pd.isnull(date_to):
+        date_to = data.index[-1]
     elif (pd.to_datetime(date_to, format="%Y-%m-%d %H:%M").time() == datetime.time(0)) and len(date_to) <= 10:
         date_to = pd.to_datetime(date_to, format="%Y-%m-%d") - datetime.timedelta(seconds=1)
     else:
         date_to = pd.to_datetime(date_to, format="%Y-%m-%d")
-
-    if pd.isnull(date_from):
-        date_from = data.index[0]
-
-    if pd.isnull(date_to):
-        date_to = data.index[-1]
 
     if date_to < date_from:
         raise ValueError('date_to must be greater than date_from')
