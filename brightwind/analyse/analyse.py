db81c1d381831476cdb44985e1922180d807661b
BiancaMorandi <biamorandi@gmail.com>
2023-05-23 17:14:27 +0100

    iss #385 update doctring of functions using slice_data

--- brightwind/analyse/analyse.py
+++ brightwind/analyse/analyse.py
@@ -377,7 +377,7 @@ def _mean_of_monthly_means_seasonal_adjusted(var_series):
     return result
 
 
-def momm(data, date_from: str = '', date_to: str = '', seasonal_adjustment=False, coverage_threshold=None):
+def momm(data, date_from=None, date_to=None, seasonal_adjustment=False, coverage_threshold=None):
     """
     Calculates and returns the mean of monthly mean speed. This accepts a DataFrame with timestamps as index column and
     another column with wind speed. You can also specify date_from and date_to to calculate the mean of monthly
@@ -399,9 +399,17 @@ def momm(data, date_from: str = '', date_to: str = '', seasonal_adjustment=False
 
     :param data:                Pandas DataFrame or Series with timestamp as index and a column with wind speed
     :type data:                 pandas.DataFrame or pandas.Series
-    :param date_from:           Start date as string in format YYYY-MM-DD
+    :param date_from:           Start date as string in format YYYY-MM-DD or YYYY-MM-DD HH:DD. If format of date_from
+                                is YYYY-MM-DD, then the first timestamp of the date is used
+                                (e.g if date_from=2023-01-01 then 2023-01-01 00:00 is the first timestamp of the
+                                sliced data). If date_from is not given then the sliced data are taken up to the
+                                first timestamp of the dataset.
     :type:                      str
-    :param date_to:             End date as string in format YYYY-MM-DD
+    :param date_to:             End date as string in format YYYY-MM-DD or YYYY-MM-DD HH:DD. If format date_to is
+                                YYYY-MM-DD, then the last timestamp of the previous day is used
+                                (e.g if date_to=2023-02-01 then 2023-01-31 23:50 is the last timestamp of the
+                                sliced data). If date_to is not given then the sliced data are taken up to the
+                                last timestamp of the dataset.
     :type:                      str
     :param seasonal_adjustment: Optional, False by default. If True, returns the mean of monthly mean seasonally
                                 adjusted
