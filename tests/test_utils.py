8baa3e3659c7cba75a33138bb14d39ad2f704644
BiancaMorandi <biamorandi@gmail.com>
2023-05-23 15:05:33 +0100

    iss #385 added test_utils

---
+++ tests/test_utils.py
@@ -0,0 +1,23 @@
+import pytest
+import brightwind as bw
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+
+DATA = bw.load_csv(bw.demo_datasets.demo_data)
+DATA = bw.apply_cleaning(DATA, bw.demo_datasets.demo_cleaning_file)
+WSPD_COLS = ['Spd80mN', 'Spd80mS', 'Spd60mN', 'Spd60mS', 'Spd40mN', 'Spd40mS']
+WDIR_COLS = ['Dir78mS', 'Dir58mS', 'Dir38mS']
+
+
+def test_slice_data():
+    data_sliced = bw.utils.utils.slice_data(DATA, date_from='2016-11-23 00:30', date_to='2017-10-23 12:20')
+
+    assert data_sliced.index[0] == pd.Timestamp('2016-11-23 00:30')
+    assert data_sliced.index[-1] == pd.Timestamp('2017-10-23 12:20')
+
+    data_sliced = bw.utils.utils.slice_data(DATA, date_from='2016-11-23', date_to='2017-10-23')
+
+    assert data_sliced.index[0] == pd.Timestamp('2016-11-23 00:00')
+    assert data_sliced.index[-1] == pd.Timestamp('2017-10-22 23:50')
+
