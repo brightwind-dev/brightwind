6da3d54fed2415649bb9a6da921c078c30b05fd3
unknown <stephen@brightwindanalysis.com>
2023-11-01 18:24:30 +0000

    iss #412 clean up according to PEP8

--- brightwind/analyse/correlation.py
+++ brightwind/analyse/correlation.py
@@ -18,7 +18,8 @@ __all__ = ['']
 
 class CorrelBase:
     def __init__(self, ref_spd, target_spd, averaging_prd, coverage_threshold=None, ref_dir=None, target_dir=None,
-                 sectors=12, direction_bin_array=None, ref_aggregation_method='mean', target_aggregation_method='mean', forced_intercept=None):
+                 sectors=12, direction_bin_array=None, ref_aggregation_method='mean', target_aggregation_method='mean',
+                 forced_intercept=None):
 
         self.ref_spd = ref_spd
         self.ref_dir = ref_dir
@@ -408,7 +409,8 @@ class OrdinaryLeastSquares(CorrelBase):
 
     """
     def __init__(self, ref_spd, target_spd, averaging_prd, coverage_threshold=0.9, ref_dir=None, sectors=12,
-                 direction_bin_array=None, ref_aggregation_method='mean', target_aggregation_method='mean', forced_intercept=None):
+                 direction_bin_array=None, ref_aggregation_method='mean', target_aggregation_method='mean',
+                 forced_intercept=None):
         CorrelBase.__init__(self, ref_spd, target_spd, averaging_prd, coverage_threshold, ref_dir=ref_dir,
                             sectors=sectors, direction_bin_array=direction_bin_array,
                             ref_aggregation_method=ref_aggregation_method,
@@ -419,33 +421,33 @@ class OrdinaryLeastSquares(CorrelBase):
 
     @staticmethod
     def _leastsquare(ref_spd, target_spd, forced_intercept):
-        if forced_intercept==True:
-            x = np.nan_to_num(ref_spd.values.flatten()[:, np.newaxis] )
+        if forced_intercept:
+            x = np.nan_to_num(ref_spd.values.flatten()[:, np.newaxis])
             y = np.nan_to_num(target_spd.values.flatten())
             p, res = lstsq(x, y)[0:2]
             r2 = 1 - res / (y.size * y.var())
             return p[0], r2
         else:
             p, res = lstsq(np.nan_to_num(ref_spd.values.flatten()[:, np.newaxis] ** [1, 0]),
-                            np.nan_to_num(target_spd.values.flatten()))[0:2]
+                           np.nan_to_num(target_spd.values.flatten()))[0:2]
             return p[0], p[1]
 
     def run(self, show_params=True):
         if self.ref_dir is None:
-            if self.forced_intercept==True:
+            if self.forced_intercept:
                 slope, r2 = self._leastsquare(ref_spd=self.data[self._ref_spd_col_name],
-                                    target_spd=self.data[self._tar_spd_col_name], 
-                                    forced_intercept=self.forced_intercept)
+                                              target_spd=self.data[self._tar_spd_col_name],
+                                              forced_intercept=self.forced_intercept)
                 offset = 0
                 self.params = dict([('slope', slope), ('offset', offset)])
                 self.params['r2'] = r2
             else:
                 slope, offset = self._leastsquare(ref_spd=self.data[self._ref_spd_col_name],
-                                              target_spd=self.data[self._tar_spd_col_name], 
-                                              forced_intercept=self.forced_intercept)
+                                                  target_spd=self.data[self._tar_spd_col_name],
+                                                  forced_intercept=self.forced_intercept)
                 self.params = dict([('slope', slope), ('offset', offset)])
                 self.params['r2'] = self._get_r2(target_spd=self.data[self._tar_spd_col_name],
-                                    predict_spd=self._predict(ref_spd=self.data[self._ref_spd_col_name]))
+                                                 predict_spd=self._predict(ref_spd=self.data[self._ref_spd_col_name]))
             
             self.params['num_data_points'] = self.num_data_pts
         elif type(self.ref_dir) is pd.Series:
@@ -455,7 +457,8 @@ class OrdinaryLeastSquares(CorrelBase):
                 # print('Processing sector:', sector)
                 if len(group) > 1:
                     slope, offset = self._leastsquare(ref_spd=group[self._ref_spd_col_name],
-                                                      target_spd=group[self._tar_spd_col_name], forced_intercept=self.forced_intercept)
+                                                      target_spd=group[self._tar_spd_col_name],
+                                                      forced_intercept=self.forced_intercept)
                     predict_ref_spd_sector = self._predict(ref_spd=group[self._ref_spd_col_name],
                                                            slope=slope, offset=offset)
                     r2 = self._get_r2(target_spd=group[self._tar_spd_col_name],
