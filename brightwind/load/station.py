413e878b01721b4627d5ba3c7d8eb3d3d46fa8f4
BiancaMorandi <biamorandi@gmail.com>
2025-03-20 21:33:05 +0000

    iss #453 updated bug fix

--- brightwind/load/station.py
+++ brightwind/load/station.py
@@ -480,14 +480,11 @@ class MeasurementStation:
 
     def __get_properties(self):
         meas_loc_prop = []
-        if self.type == 'mast':
+        if self.type in ['mast', 'solar']:
             meas_loc_prop = _flatten_dict(self.__meas_loc_data_model, property_to_bring_up='mast_properties')
         elif self.type in ['lidar', 'sodar', 'floating_lidar']:
             meas_loc_prop = _flatten_dict(self.__meas_loc_data_model,
                                           property_to_bring_up='vertical_profiler_properties')
-        elif self.type == 'solar':
-            meas_loc_prop = _flatten_dict(self.__meas_loc_data_model,
-                                          property_to_bring_up='sensor')
         return meas_loc_prop
 
     def get_table(self, horizontal_table_orientation=False):
