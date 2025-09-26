f0d6c71e10742bd293229cf5a89e130cae1dd6b6
Sara Rafter <sararafter@users.noreply.github.com>
2025-09-26 16:23:24 +0100

    iss #535 attempt to avoid breaking change

--- tests/test_analyse.py
+++ tests/test_analyse.py
@@ -643,10 +643,16 @@ def test_calc_air_density():
         bw.calc_air_density(15, 1013, elevation_ref_m=200)
     assert str(except_info.value) == 'Specify value of elevation_site_m (float)'
     assert abs(bw.calc_air_density(15, 1013) - 1.225) < 1e-3
-    assert abs(bw.calc_air_density(15, 1013, elevation_ref_m=0, elevation_site_m=200) - 1.20211) < 1e-3
+    assert abs(bw.calc_air_density(15, 1013, elevation_ref_m=0, elevation_site_m=200) - 1.203) < 1e-3
     assert (abs(bw.calc_air_density(pd.Series([15, 12.5, -5, 23]), pd.Series([1013, 990, 1020, 900])) -
                 pd.Series([1.225, 1.208, 1.326, 1.059])) < 1e-3).all()
-
+    assert abs(bw.calc_air_density(15, 1013, specific_gas_constant=287.05) - 1.22471) < 1e-3
+    assert abs(bw.calc_air_density(15, 1013, 50) - 1.22093) < 1e-3
+    assert abs(bw.calc_air_density(15, 1013, 50, elevation_ref_m=0, elevation_site_m=200) - 1.19833) < 1e-3
+    assert (abs(bw.calc_air_density(DATA.T2m, DATA.P2m).tail(5).values -
+                pd.Series([1.19918, 1.19984, 1.19979, 1.19944, 1.20107])) < 1e-3).all()
+    assert (abs(bw.calc_air_density(DATA.T2m, DATA.P2m, DATA.RH2m).tail(5).values -
+                pd.Series([1.19529, 1.19601, 1.19592, 1.19555, 1.19719])) < 1e-3).all()
 
 def test_dist_matrix_by_direction_sector():
     bw.dist_matrix_by_dir_sector(var_series=DATA.Spd80mN, var_to_bin_by_series=DATA.Spd80mN,
