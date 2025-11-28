72ce4788596b39eb1079d31183b624f6300d1c25
Olivia Bentley <olivia.bentley@brightwindanalysis.com>
2025-11-28 12:21:41 +0000

    iss #521 revised grouping of rows logic

--- brightwind/transform/transform.py
+++ brightwind/transform/transform.py
@@ -1678,9 +1678,31 @@ def apply_device_orientation_offset(
         data.update(df)
 
     if return_results_table:
-        results_table = pd.DataFrame(rows).sort_values(
+        results_df = pd.DataFrame(rows).sort_values(
             by=["Height [m]", "Date From"], ascending=[False, True]
-            ).set_index("Name")
+            )
+        results_df['consecutive_group'] = (
+            (results_df['Name'] != results_df['Name'].shift()) |
+            (results_df['Height [m]'] != results_df['Height [m]'].shift()) |
+            (results_df['Device Orientation [deg]'] != results_df['Device Orientation [deg]'].shift()) |
+            (results_df['Logger Offset'] != results_df['Logger Offset'].shift()) |
+            (results_df['Offset applied [deg]'] != results_df['Offset applied [deg]'].shift())
+            ).cumsum()
+        
+        # Group and aggregate consecutive periods
+        results_table = results_df.groupby([
+            'Name',
+            'consecutive_group',
+            'Height [m]',
+            'Device Orientation [deg]',
+            'Logger Offset',
+            'Offset applied [deg]'
+            ]).agg({
+            'Date From': 'first',
+            'Date To': lambda x: None if any(d is None for d in x) else max(x)
+            }).reset_index(drop=False).drop(columns=['consecutive_group']).set_index("Name").sort_values(
+                by=["Height [m]", "Date From"], ascending=[False, True]
+                )
         return df, results_table
     return df
 
