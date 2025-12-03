3e86cd43ff833d860cde98cd2a1c8252613d6321
sararafter <183604246+sararafter@users.noreply.github.com>
2025-12-03 15:25:17 +0000

    iss #504 updated to add 1 second to the last timestamp instead of 10mins when no date_to given

--- brightwind/transform/transform.py
+++ brightwind/transform/transform.py
@@ -1441,9 +1441,9 @@ def offset_timestamps(data, offset, date_from=None, date_to=None, overwrite=Fals
 
     if pd.isnull(date_to):
         if isinstance(data, pd.DatetimeIndex):
-            date_to = data[-1] + pd.DateOffset(minutes=10)
+            date_to = data[-1] + pd.DateOffset(seconds=1)
         elif isinstance(data, pd.Series) or isinstance(data, pd.DataFrame):
-            date_to = data.index[-1] + pd.DateOffset(minutes=10)
+            date_to = data.index[-1] + pd.DateOffset(seconds=1)
     else:
         date_to = pd.to_datetime(date_to)
 
