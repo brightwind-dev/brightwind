0f6606512c31c304b8f99b551e9a441c4897db24
sararafter <183604246+sararafter@users.noreply.github.com>
2026-01-16 17:05:41 +0000

    iss #539 moved constants to utils/constants.py

--- brightwind/analyse/analyse.py
+++ brightwind/analyse/analyse.py
@@ -5,8 +5,8 @@ from brightwind.transform import transform as tf
 from brightwind.utils import utils
 from brightwind.analyse import plot as bw_plt
 from brightwind.transform.scale import scale_air_density_to_height
-from brightwind.utils.utils import _convert_df_to_series, assert_function_input_type
-from brightwind.utils.utils import validate_coverage_threshold
+from brightwind.utils.utils import _convert_df_to_series, assert_function_input_type, validate_coverage_threshold
+from brightwind.utils.constants import GAS_CONST_DRY_AIR, GAS_CONST_WATER, AIR_DENSITY_LAPSE_RATE
 from brightwind.export.export import _calc_mean_speed_of_freq_tab
 import matplotlib.pyplot as plt
 import warnings
@@ -31,14 +31,6 @@ __all__ = ['monthly_means',
            'sector_ratio',
            'calc_air_density']
 
-#  Specific gas constant for dry air (J/K/kg or m2/K/s2) from ISO:2533-1975 Standard Atmosphere
-GAS_CONST_DRY_AIR = 287.05
-
-#  Specific gas constant for water vapour (J/K/kg or m2/K/s2)
-GAS_CONST_WATER = 461.495
-
-# Air density lapse rate (kg/m3/km) from WindFarmer Theory Manual Version 5.3, DNV GL (April 2014)
-AIR_DENSITY_LAPSE_RATE = -0.113
 
 def dist_matrix(var_series, x_series, y_series,
                 num_bins_x=None, num_bins_y=None,
